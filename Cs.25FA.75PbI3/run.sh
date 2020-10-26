16aea14c0f41e4510b1cc56e33781ee8f7a7c70a
QingmuDeng <qdeng@olin.edu>
2020-10-25 20:41:52 -0400

    9 k-point not 10

--- Cs.25FA.75PbI3/run.sh
+++ Cs.25FA.75PbI3/run.sh
@@ -2,7 +2,7 @@ mpiexec -np 4 python3 gpaw_relax_5.py
 mpiexec -np 4 python3 gpaw_relax_6.py
 # mpiexec -np 4 python3 gpaw_relax_7.py
 mpiexec -np 4 python3 gpaw_relax_8.py
-mpiexec -np 4 python3 gpaw_relax_10.py
+mpiexec -np 4 python3 gpaw_relax_9.py
 
 # mpiexec -np 4 python3 gpaw_relax_380.py
 # mpiexec -np 4 python3 gpaw_relax_410.py
