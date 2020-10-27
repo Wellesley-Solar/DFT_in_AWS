733194774e284d1e6bfb406d07c8d71f4d281d48
QingmuDeng <qdeng@olin.edu>
2020-10-27 11:03:52 -0400

    convergence 800 and 900

--- Cs.25FA.75PbI3/run.sh
+++ Cs.25FA.75PbI3/run.sh
@@ -4,7 +4,9 @@
 # mpiexec -np 4 python3 gpaw_relax_8.py
 # mpiexec -np 4 python3 gpaw_relax_9.py
 
-mpiexec -np 4 python3 gpaw_relax_380.py
-mpiexec -np 4 python3 gpaw_relax_410.py
+# mpiexec -np 4 python3 gpaw_relax_380.py
+# mpiexec -np 4 python3 gpaw_relax_410.py
 # mpiexec -np 4 python3 gpaw_relax_440.py
-mpiexec -np 4 python3 gpaw_relax_500.py
+# mpiexec -np 4 python3 gpaw_relax_500.py
+mpiexec -np 4 python3 gpaw_relax_800.py
+mpiexec -np 4 python3 gpaw_relax_900.py
